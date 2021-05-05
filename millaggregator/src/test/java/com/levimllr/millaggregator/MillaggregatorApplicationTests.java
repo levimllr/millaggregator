103adec5c0e1bef96c13412f57c38e720947e5e5
Levi Miller <paullevimiller@gmail.com>
2021-05-05 16:47:12 -0700

    build: spring starter app

---
+++ millaggregator/src/test/java/com/levimllr/millaggregator/MillaggregatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levimllr.millaggregator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MillaggregatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
